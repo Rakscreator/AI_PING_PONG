36a45c568d78d5e31e46ae29e46cd61337b1c2a6
Rakshit Malik <rakshitmalik156@gmail.com>
2024-03-05 13:11:56 +0530

    asdda

--- main.js
+++ main.js
@@ -200,6 +200,17 @@ function models(){
     text("Height:"+height,235,15)
 }
 
+//this function help to not go te paddle out of canvas
+function paddleInCanvas(){
+  if(paddle1Y+paddle1Height > height){
+    paddle1Y=height-paddle1Height;
+  }
+  if(paddle1Y < 0){
+    paddle1Y =0;
+  }
+ 
+  
+}
 
 function preload(){
   ball_touch_paddel = loadSound("ball_touch_paddel.wav");
